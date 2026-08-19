acb688ca606a6c57d7be26238d2fa11fa035945d
Leonzmy <leon.zmelty@gmx.de>
2026-08-19 09:36:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -34,6 +34,8 @@ const CONFIG = {
 
     // Programmheft
   PROGRAMMHEFT_SHEET_NAME: "Programmheft",
+    // Blatt für die Programmreihenfolge (Stücke je Event)
+  PROGRAMM_SHEET_NAME: "Programm",
   // Maximale Zeichenzahl für Kurzbios (gilt im öffentlichen Formular)
   BIO_MAX_CHARS: 800,
   // URL, unter der programmheft-formular.html erreichbar ist
